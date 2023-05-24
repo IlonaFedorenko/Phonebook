1efb2ebc7bd7ddef3dcf4284647d0d4fbd17a2f2
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-05-24 19:51:20 +0000

    Deploying to gh-pages from @ IlonaFedorenko/goit-react-hw-08-phonebook@206efc567b5e7beccc56f2fb6fc383b86b5a57b1 🚀

---
+++ static/js/936.4fba492d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{936:function(e,t,o){o.r(t),o.d(t,{default:function(){return c}});var n="Home_home__CMw1J",r=o(329),c=function(){return(0,r.jsx)("div",{className:n,children:(0,r.jsx)("h1",{children:"Welcome to PhoneBook App"})})}}}]);
+//# sourceMappingURL=936.4fba492d.chunk.js.map
